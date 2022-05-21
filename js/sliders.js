08acb50ed1f38badaa63bd0ea7fd6c1b5139d131
mohammedbahnini <63128845+mohammedbahnini@users.noreply.github.com>
2022-05-21 16:48:21 +0100

    fixing issues

--- js/sliders.js
+++ js/sliders.js
@@ -1,5 +1,6 @@
 $(document).ready( ()=>{
-    $('.team .members').slick({
+
+    const options = {
         slidesToShow: 3,
         infinite: true,
         dots: false,
@@ -25,35 +26,11 @@ $(document).ready( ()=>{
             } 
             
     ]
-    });
+    };
+
+    $('.team .members').slick(options);
 
 
-    $('.ambassadors .members').slick({
-        slidesToShow: 3,
-        infinite: true,
-        dots: false,
-        prevArrow: `<button class="prev-slide"></button>`,
-        nextArrow: `<button class="next-slide"></button>`,
-        responsive: [
-            {
-                breakpoint : 1200 , 
-                settings : {
-                    slidesToShow : 2 , 
-                    arrows : true , 
-                    dots : true 
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    slidesToShow: 1,
-                    arrows: false,
-                    centerMode: false
-                }
-            } 
-            
-    ]
-    });
+    $('.ambassadors .members').slick(options);
 
 });
